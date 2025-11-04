2acdddd8ea9e43a8aebf78f11622a2ff20374a71
651310117 <Nantaporn.251146@gmail.com>
2025-11-04 20:11:37 +0700

    can move scene

---
+++ Assets/Scripts/MapTransition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MapTransition : MonoBehaviour
+{
+    public string nextMapName;     // Scene ปลายทาง
+    public string spawnPointName;  // ชื่อ SpawnPoint ใน Scene ใหม่
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneLoader.Instance.LoadMap(nextMapName, spawnPointName);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        DontDestroyOnLoad(gameObject); // Player อยู่รอดทุกซีน
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+}
