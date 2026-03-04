97c20dd66482e18e97ebeafe25dc9da0a3e5bff4
651310117 <Nantaporn.251146@gmail.com>
2026-03-05 00:37:13 +0700

    table in hall

---
+++ Assets/Scripts/โต๊ะเลื่อน/MovingTable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MovingTable : MonoBehaviour
+{
+    public Transform targetPoint;
+    public float speed = 3f;
+
+    private bool move = false;
+
+    void Update()
+    {
+        if (!move) return;
+
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            targetPoint.position,
+            speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, targetPoint.position) < 0.01f)
+        {
+            move = false;   // หยุดถาวร
+        }
+    }
+
+    public void StartMove()
+    {
+        move = true;
+    }
+}
---
+++ Assets/Scripts/โต๊ะเลื่อน/TableTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TableTrigger : MonoBehaviour
+{
+    [Header("Tables To Move")]
+    public MovingTable[] tables;   // ใส่ได้หลายตัวใน Inspector
+
+    private bool activated = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !activated)
+        {
+            activated = true;
+
+            foreach (MovingTable table in tables)
+            {
+                table.StartMove();
+            }
+        }
+    }
+}
